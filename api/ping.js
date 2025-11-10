6478d4e9d771e39e4da0c5a7557c76404ff8a677
dmitrsav <saveliev2021@icloud.com>
2025-11-10 17:31:47 +0300

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,3 +1,6 @@
-export default async function handler(req, res) {
-  res.status(200).json({ ok: true, time: new Date().toISOString() });
-}
+module.exports = (req, res) => {
+  res.status(200).json({
+    ok: true,
+    time: new Date().toISOString()
+  });
+};
